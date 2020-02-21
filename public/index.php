22144aeb240b409a06cba9f0eb3e47fa2977178a
Ron <ron@valhallaonlineservices.com>
2020-02-21 12:38:44 +0800

    edited auth.php

--- public/index.php
+++ public/index.php
@@ -3,6 +3,8 @@
 // header('Access-Control-Allow-Origin: *');
 // header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 // header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+
+
 /*
 |--------------------------------------------------------------------------
 | Create The Application
