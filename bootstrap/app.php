7b9259fbd9e5330ce6afa5eb441de8dad67bbbbc
Ron <ron@valhallaonlineservices.com>
2020-02-21 11:57:07 +0800

    Try fixing Cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -59,8 +59,7 @@ $app->singleton(
 */
 
 $app->middleware([
-    //App\Http\Middleware\ExampleMiddleware::class
-    App\Http\Middleware\CorsMiddleware::class
+        App\Http\Middleware\CorsMiddleware::class
 ]);
 
 $app->routeMiddleware([
